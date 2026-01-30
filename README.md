52e34efae49e5ffe63946e14c45c6b68ed2326a4
Kissu <kissu@nixos>
2026-01-31 00:24:49 +0530

    activitywatch

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Super: flag/logo key
 
 Alt: not using absolutely anywhere for wm
 
-# i3
-## Flameshot
+# i3/sway
+## Screenshot:
 - Print: Select region
 - Super + Print: Fullscreen
 - Super + Shift + Print : Select region and copy to clipboard without saving
+
+---
+
+- Ram test:
+```nix
+boot.loader.systemd-boot.memtest86.enable = true;
+```
---
+++ modules/activity_watcher.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    activitywatch
+    awatcher
+  ];
+
+  # background service for activity watch server
+  systemd.user.services.activitywatch = {
+    description = "ActivityWatch Server";
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.activitywatch}/bin/aw-server";
+      Restart = "on-failure";
+    };
+  };
+
+  # running awatcher as a service too
+  systemd.user.services.awatcher = {
+    description = "ActivityWatch Wayland Watcher";
+    after = [ "activitywatch.service" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.awatcher}/bin/awatcher";
+      Restart = "on-failure";
+    };
+  };
+}
