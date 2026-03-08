5d03fa0dbc2c78de8c3f2debfe192dff9cd77288
Kissu <kissu@nixos>
2026-03-08 19:11:01 +0530

    Updated battery settings to be more chill when on battery only

--- machine/hp_laptop/battery.nix
+++ machine/hp_laptop/battery.nix
@@ -17,16 +17,17 @@
         WOL_DISABLE = "Y";
 
         TLP_PERSISTENT_DEFAULT = 0;
-        TLP_DEFAULT_MODE = "AC";
+        TLP_DEFAULT_MODE = "BAL";
 
+        # enable boost on AC
         INTEL_TURBO_BOOST_ON_AC = 0;
         INTEL_TURBO_BOOST_ON_BAT = 0;
 
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
 
         CPU_BOOST_ON_AC = 0;
         CPU_BOOST_ON_BAT = 0;
